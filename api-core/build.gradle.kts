9ad5e6be872892de57a2cb67f031f5c95520d955
foxmon <jjockrod@naver.com>
2024-05-02 00:13:16 +0900

    settings

--- api-core/build.gradle.kts
+++ api-core/build.gradle.kts
@@ -1,9 +1,15 @@
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    // mysql
+    runtimeOnly("com.mysql:mysql-connector-j")
 }
 
 tasks.bootJar {
-    enabled = false
+    enabled = true
 }
 
 tasks.jar {
